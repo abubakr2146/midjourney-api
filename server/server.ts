590d5ea8b5b3952137b60e2972b91a412b3758bd
abubakr2146 <124148219+abubakr2146@users.noreply.github.com>
2024-05-06 18:49:44 +0500

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -5,12 +5,19 @@ const app = express();
 const port = 3000;
 
 app.get('/imagine', (req: Request, res: Response) => {
+  const prompt = req.query.prompt as string;
+
+  if (!prompt) {
+    res.status(400).send('Missing prompt parameter');
+    return;
+  }
+
   imagineScenario(
     process.env.SERVER_ID as string,
     process.env.CHANNEL_ID as string,
     process.env.SALAI_TOKEN as string,
     process.env.HUGGINGFACE_TOKEN as string,
-    "a cow riding a bus"
+    prompt
   )
     .then(() => {
       res.send("finished");
